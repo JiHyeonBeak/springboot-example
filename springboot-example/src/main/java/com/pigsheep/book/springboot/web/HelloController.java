b900f71bcadf48924a7befa4825ad412a595b139
jh <jadehl09@gmail.com>
2024-02-26 20:57:31 +0900

    bug fix, add dto test

--- springboot-example/src/main/java/com/pigsheep/book/springboot/web/HelloController.java
+++ springboot-example/src/main/java/com/pigsheep/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.pigsheep.book.springboot.web;
 
+import com.pigsheep.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // JSON 반환 컨트롤러를 선언한다.
@@ -9,6 +11,11 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello";
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,@RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
--- springboot-example/src/test/java/springboot/web/HelloControllerTest.java
+++ springboot-example/src/test/java/springboot/web/HelloControllerTest.java
@@ -1,13 +1,20 @@
 package springboot.web;
 
 import com.pigsheep.book.springboot.web.HelloController;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+/* ContextConfiguration
+   스프링 컨테이너에 빈을 추가하기 위한 설정 파일 로드를 위한 어노테이션.
+   객체를 직접 만들지 않으므로 붙여준다. */
+@ContextConfiguration(classes = HelloController.class)
 @WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
 
@@ -21,5 +28,18 @@ public class HelloControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().string(hello));
 
+    }
+
+    @Test
+    public void return_helloDto() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello/dto")
+                .param("name",name)
+                .param("amount",String.valueOf(amount)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.is(name)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.amount", Matchers.is(amount)));
     }
 }
