f7a3042024bfd1040ad23791480199d4f447ace3
jh <jadehl09@gmail.com>
2024-03-01 17:51:06 +0900

    add createPosts

---
+++ springboot-example/src/main/java/com/pigsheep/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,21 @@
+package com.pigsheep.book.springboot.service.posts;
+
+import com.pigsheep.book.springboot.domain.posts.PostsRepository;
+import com.pigsheep.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.beans.Transient;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
