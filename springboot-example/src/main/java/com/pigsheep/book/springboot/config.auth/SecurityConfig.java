4e441a896df522e1840a082919885169fc9579cd
jh <jadehl09@gmail.com>
2024-03-24 21:03:45 +0900

    add dependency

---
+++ springboot-example/src/main/java/com/pigsheep/book/springboot/config.auth/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.pigsheep.book.springboot.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
