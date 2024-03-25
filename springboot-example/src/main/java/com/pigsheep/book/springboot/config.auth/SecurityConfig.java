33b5d5b0e5ba7f77593f8b5c74f1e1eabe5bf655
jh <jadehl09@gmail.com>
2024-03-25 22:07:35 +0900

    add SpringSecurity 2

--- springboot-example/src/main/java/com/pigsheep/book/springboot/config.auth/SecurityConfig.java
+++ springboot-example/src/main/java/com/pigsheep/book/springboot/config.auth/SecurityConfig.java
@@ -1,11 +1,30 @@
 package com.pigsheep.book.springboot.config.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
-@EnableWebSecurity
+@EnableWebSecurity  // Spring Security 활성화
+// Spring Security 5.7부터 WebSecurityConfigurerAdapter Deprecate
 public class SecurityConfig {
+
+    /*
+        예외처리가 필요한 url 등록
+     */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers("/");
+    }
+
+    @Bean
+    protected SecurityFilterChain webSecurityFilterChain(HttpSecurity http) throws Exception {
+    }
+
+
 
 }
